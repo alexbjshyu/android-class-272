0d3721031f23b28bf79b264649f421f3b55775e7
alexbjshyu <alexbjshyu@gmail.com>
2016-08-10 21:21:59 +0800

    add adapter stuff and list

--- SimpleUI/app/src/main/java/com/example/familyfirst/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/familyfirst/simpleui/MainActivity.java
@@ -3,17 +3,30 @@ package com.example.familyfirst.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+    Spinner spinner;
+    Order order;
 
     String drink = "Black Tea";
+
+    List<Order> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         //textView.setText("Hello Android!!!!");
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -38,19 +53,71 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Order order = (Order)parent.getAdapter().getItem(position);
+                Toast.makeText(MainActivity.this,order.note,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setupListView();
+        setupSpinner();
     }
 
+    private void setupSpinner()
+    {
+        String[] storeInfo = getResources().getStringArray(R.array.storeInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,storeInfo);
+                spinner.setAdapter(adapter);
+    }
 
+    private void setupListView()
+    {
+       // String[] data = new String[]{"1","2","3","4","5","6","7"};
+
+//        List<Map<String, String>> mapList = new ArrayList<>();
+//
+//        for(Order order : data)
+//        {
+//            Map<String, String> item = new HashMap<>();
+//
+//            item.put("note", order.note);
+//            item.put("storeInfo",order.storeInfo);
+//            item.put("drink",order.drink);
+//
+//            mapList.add(item);
+//        }
+//
+//        String[] from = {"note","storeInfo","drink"};
+//        int[] to = {R.id.noteTextView,R.id.storeInfoTextView,R.id.drinkTextView};
+//
+//        SimpleAdapter adapter = new SimpleAdapter(this,mapList,R.layout.listview_order_item,from,to);
+
+          OrderAdapter adapter = new OrderAdapter(this,data);
+          listView.setAdapter(adapter);
+    }
 
 
     public void click(View view)
     {
         String text = editText.getText().toString();
+        String result = text+"Order: " + drink;
 
-        text = text+" Order: " + drink;
-        textView.setText(text);
+
+        textView.setText(result);
 
         editText.setText("");
+
+        Order order = new Order();
+
+        order.note = text;
+        order.drink = drink;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+        data.add(order);
+        setupListView();
 
     }
 }
---
+++ SimpleUI/app/src/main/java/com/example/familyfirst/simpleui/Order.java
@@ -0,0 +1,10 @@
+package com.example.familyfirst.simpleui;
+
+/**
+ * Created by familyfirst on 8/10/16.
+ */
+public class Order {
+    String note;
+    String storeInfo;
+    String drink;
+}
